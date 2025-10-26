02b6383b935b52fcb43587cc788e422fa231aaf2
wikipoz000-netizen <wikipoz000@gmail.com>
2025-10-26 19:07:57 +0100

    Add ResourceManager class with resource management
    Implemented ResourceManager with copy constructor and assignment operator.

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,27 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource_;
+public:
+    ResourceManager() 
+        : resource_(new Resource()){}
+
+    ~ResourceManager(){
+        delete resource_;
+}
+
+    ResourceManager(const ResourceManager& other)
+        : resource_(new Resource(*other.resource_)){}
+
+    ResourceManager& operator=(const ResourceManager& other){
+        if (this != &other)
+        {
+            Resource* newResource = new Resource(*other.resource_);
+            delete resource_;
+            resource_ = newResource;
+        }
+        return *this;
+    }
+
 };
